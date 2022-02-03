15ba6894c8f7fda2158dbebe7e445b202252e708
lijindanieljoseph <lijjindaniejoseph@gmail.com>
2022-02-03 13:44:43 +0530

    initial commit

---
+++ prog4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<ctype.h>
+void main()
+{
+char a[10];
+int flag, i=1;
+printf("\n Enter an identifier:");
+scanf("%s",a);
+if(isalpha(a[0])||a[0]=='_')
+  flag=1;
+else
+{
+ flag=0;
+}
+while(a[i]!='\0')
+{
+  if(!isdigit(a[i])&&!isalpha(a[i])&&a[i]!='_')
+   {
+     flag=0;
+     break;
+   }
+i++;
+}
+if(flag==1)
+printf("\n Valid identifier");
+else
+printf("\n not a valid identifier");
+}
